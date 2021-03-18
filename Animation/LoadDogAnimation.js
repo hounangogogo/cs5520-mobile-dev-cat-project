c7078e494ba72909a3c694c2538e118da199b596
hounangogogo <1294030303@qq.com>
2021-03-17 23:04:43 -0700

    add loading animation

---
+++ Animation/LoadDogAnimation.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+
+class LoadDogAnimation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <View>
+                <Animatable.Image
+                    style={styles.img}
+                    source={require('../assets/img/dog.png')} 
+                    iterationCount={3}
+                    animation = {{
+                        0: {
+                            opacity: 0,
+                            width: 100,
+                            height: 100
+                        },
+                        0.5: {
+                            opacity: 1,
+                            height: 150,
+                            width: 150
+                        },
+                        1: {
+                            opacity: 0,
+                            width: 100,
+                            height: 100
+                        }
+                    }}
+                    />
+
+            
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    img: {
+        width: 150,
+        height: 150
+    }
+})
+
+export default LoadDogAnimation;
