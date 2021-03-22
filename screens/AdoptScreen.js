3f099230d4bfe85ec4b8f60023ea7e12ed255243
JunyanLing1 <ling.ju@northeastern.edu>
2021-03-21 21:52:18 -0700

    updated questiondog partially

--- screens/AdoptScreen.js
+++ screens/AdoptScreen.js
@@ -8,16 +8,30 @@ import {
   Button,
   TouchableOpacity,
   Animated,
+  Dimensions,
+  ScrollView,
+  SafeAreaView
 } from "react-native";
 import { Slider, Icon } from "react-native-elements";
 import { connect } from "react-redux";
 import * as Animatable from "react-native-animatable";
+import DragSortableView from './DragSortableView';
+import { CheckBox } from 'react-native-elements';
+
+import { TEST_DATA } from './data';
+
+const { width } = Dimensions.get('window')
+
+const parentWidth = width
+const childrenWidth = width
+const childrenHeight = 48
 
 const AnimatableTouch = Animatable.createAnimatableComponent(TouchableOpacity);
 
 class AdoptScreen extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       chooseDog: true,
 
@@ -31,12 +45,32 @@ class AdoptScreen extends Component {
       hairless: 5,
 
       // for dog
+      size: 2,
+      checkedWorking: false,
+      checkedSporting: false,
+      checkedNonSporting: false,
+      checkedHound: false,
+      checkedToy: false,
+      checkedTerrier: false,
+      checkedMixed: false,
+
+      data: TEST_DATA,
+      scrollEnabled: true,
+
     };
   }
+
   render() {
+    let s = this.state.size;
+
     console.log(this.state);
     return (
-      <View style={styles.screen}>
+      <ScrollView
+        ref={(scrollView) => this.scrollView = scrollView}
+        scrollEnabled={this.state.scrollEnabled}
+        style={styles.screen}>
+        {/* <View style={styles.screen}> */}
+
         <View style={styles.choose}>
           <AnimatableTouch
             onPress={() => {
@@ -84,75 +118,163 @@ class AdoptScreen extends Component {
           </AnimatableTouch>
         </View>
 
-        <View style={styles.form}>
-          {this.state.chooseDog ? (
-            <View>
-              <Text>Dog Form</Text>
-              <View style={styles.form}>
-                <Text style={styles.label}>Pet name</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={this.state.name}
-                  onChange={(e) => {
-                    this.setState({
-                      name: e.target.value,
-                    });
-                  }}
-                  placeholder="Pet Name"
-                />
+        {this.state.chooseDog ? (
+          <View>
+            <View style={styles.form}>
 
-                <Text style={styles.label}>Pet Color</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={this.state.color}
-                  onChange={(e) => {
-                    this.setState({
-                      color: e.target.value,
-                    });
-                  }}
-                  placeholder="Pet Color"
-                />
+              {(s < 0.5) ? <Text style={styles.label}>Choose your favorite size: Extra Small</Text> :
+                (s < 1.5) ? <Text style={styles.label}>Choose your favorite size: Small</Text> :
+                  (s < 2.5) ? <Text style={styles.label}>Choose your favorite size: Medium</Text> :
+                    (s < 3.5) ? <Text style={styles.label}>Choose your favorite size: Large</Text> :
+                      (s >= 3.5) ? <Text style={styles.label}>Choose your favorite size: Extra Large</Text> :
+                        ""
+              }
+              <Slider
+                value={this.state.size}
+                onValueChange={(size) =>
+                  this.setState({ size })
+                }
+                maximumValue={4}
+                thumbStyle={{
+                  height: 20,
+                  width: 20,
+                  backgroundColor: "transparent",
+                }}
+                thumbProps={{
+                  children: (
+                    <Icon
+                      name="pets"
+                      size={10}
+                      reverse
+                      containerStyle={{ bottom: 10, right: 10 }}
+                      color="#54514B"
+                    />
+                  ),
+                }}
+              />
 
-                <Text style={styles.label}>Pet Breeds</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={this.state.breeds}
-                  onChange={(e) => {
-                    this.setState({
-                      breeds: e.target.value,
-                    });
-                  }}
-                  placeholder="Pet Breeds"
-                />
+              <Text style={styles.label}>Choose breed groups you are interested in:</Text>
+              <CheckBox
+                left
+                title='Working'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedWorking}
+                onPress={() => this.setState({ checkedWorking: !this.state.checkedWorking })}
+              />
+              <CheckBox
+                left
+                title='Sporting'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedSporting}
+                onPress={() => this.setState({ checkedSporting: !this.state.checkedSporting })}
+              />
+              <CheckBox
+                left
+                title='Non-Sporting'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedNonSporting}
+                onPress={() => this.setState({ checkedNonSporting: !this.state.checkedNonSporting })}
+              />
+              <CheckBox
+                left
+                title='Hound'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedHound}
+                onPress={() => this.setState({ checkedHound: !this.state.checkedHound })}
+              />
+              <CheckBox
+                left
+                title='Toy'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedToy}
+                onPress={() => this.setState({ checkedToy: !this.state.checkedToy })}
+              />
+              <CheckBox
+                left
+                title='Terrier'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedTerrier}
+                onPress={() => this.setState({ checkedTerrier: !this.state.checkedTerrier })}
+              />
+              <CheckBox
+                left
+                title='Mixed'
+                iconLeft
+                iconType='material'
+                checkedIcon='clear'
+                uncheckedIcon='add'
+                checkedColor='red'
+                checked={this.state.checkedMixed}
+                onPress={() => this.setState({ checkedMixed: !this.state.checkedMixed })}
+              />
 
-                <Text style={styles.label}>Pet Species</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={this.state.species}
-                  onChange={(e) => {
-                    this.setState({
-                      species: e.target.value,
-                    });
-                  }}
-                  placeholder="Pet Species"
-                />
+              <Text style={styles.label}>Rank the temperaments you prefer:</Text>
 
-                <Text style={styles.label}>Contact</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={this.state.phone}
-                  onChange={(e) => {
-                    this.setState({
-                      phone: e.target.value,
-                    });
-                  }}
-                  placeholder="Phone"
-                />
+              <DragSortableView
+                dataSource={this.state.data}
 
-                <Button title="Submit" onPress={this.addNewLost} />
-              </View>
+                parentWidth={parentWidth}
+
+                childrenWidth={childrenWidth}
+                childrenHeight={childrenHeight}
+
+                scaleStatus={'scaleY'}
+
+                onDragStart={(startIndex, endIndex) => {
+                  this.setState({
+                    scrollEnabled: false
+                  })
+                }}
+                onDragEnd={(startIndex) => {
+                  this.setState({
+                    scrollEnabled: true
+                  })
+                }}
+                onDataChange={(data) => {
+                  if (data.length != this.state.data.length) {
+                    this.setState({
+                      data: data
+                    })
+                  }
+                }}
+                keyExtractor={(item, index) => item.txt} // FlatList作用一样，优化
+                onClickItem={(data, item, index) => {
+
+                }}
+                renderItem={(item, index) => {
+                  return this.renderItem(item, index)
+                }}
+              />
+
+              <Button title="Submit" onPress={this.addNewLost} />
             </View>
-          ) : (
+          </View>
+
+        ) : (
             <View>
               {/* Cat Form */}
               <View style={styles.form}>
@@ -182,6 +304,7 @@ class AdoptScreen extends Component {
                     ),
                   }}
                 />
+
 
                 <Text style={styles.label}>
                   AffectionLevel: {Math.round(this.state.affectionLevel, 2)}
@@ -320,10 +443,23 @@ class AdoptScreen extends Component {
               </View>
             </View>
           )}
+      </ScrollView>
+    )
+  }
+
+  renderItem(item, index) {
+    return (
+      <View style={styles.item}>
+        <View style={styles.item_children}>
+          <Image
+            style={styles.item_icon}
+            source={item.icon} />
+          <Text style={styles.item_text}> {item.txt}</Text>
         </View>
       </View>
-    );
+    )
   }
+
 }
 
 const styles = StyleSheet.create({
@@ -335,6 +471,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ddd",
   },
   choose: {
+    marginTop: 30,
     flex: 1,
     flexDirection: "row",
     justifyContent: "center",
@@ -358,6 +495,36 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
   },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  item: {
+    width: childrenWidth,
+    height: childrenHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item_children: {
+    width: childrenWidth,
+    height: childrenHeight - 4,
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+  item_icon: {
+    width: childrenHeight * 0.6,
+    height: childrenHeight * 0.6,
+    marginLeft: 15,
+    resizeMode: 'contain',
+  },
+  item_text: {
+    marginRight: 15,
+    color: 'green'
+  }
 });
 
 export default AdoptScreen;
---
+++ screens/data.js
@@ -0,0 +1,9 @@
+export const TEST_DATA = [
+    {icon: require('../assets/dog2.png'),txt: "Curious"},
+    {icon: require('../assets/dog2.png'),txt: "Intelligent"},
+    {icon: require('../assets/dog2.png'),txt: "Independent"},
+    {icon: require('../assets/dog2.png'),txt: "Dutiful"},
+    // {icon: require('../assets/dog2.png'),txt: Friendly},
+    // {icon: require('../assets/dog2.png'),txt: Loyal},
+    // {icon: require('../assets/dog2.png'),txt: Brave},
+]
